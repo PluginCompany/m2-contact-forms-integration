c73991d3a124e84f5d994c5c378d3879f9d2d487
Milan <milan@plugin.company>
2018-06-07 17:58:13 +0200

    initial commit

---
+++ app/code/PluginCompany/ContactFormsIntegration/Observer/SaveCsv.php
@@ -0,0 +1,22 @@
+<?php
+namespace PluginCompany\ContactFormsIntegration\Observer;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class SaveCsv implements ObserverInterface
+{
+
+    public function execute(Observer $observer)
+    {
+        $entry = $observer->getEvent()->getEntry();
+        $fields = $entry->getFields();
+
+        $entry->unsetData('fields');
+        $entry->unsetData('_parent_form');
+
+        $submissionData = array_merge($entry->getData(), json_decode($fields, true));
+
+        //now use $submissionData (save to CSV, call external API etc)
+
+    }
+}
